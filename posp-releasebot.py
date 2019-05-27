7103830b8a8bd20b5f6540a6c996ce11fc5e77ee
HrX03 <dn.bianco03@gmail.com>
2019-05-27 21:05:20 +0200

    Add new changelog command ewe

--- posp-releasebot.py
+++ posp-releasebot.py
@@ -26,6 +26,12 @@ def latest(bot, update):
     else:
         bot.send_message(chat_id=update.message.chat_id, text="Device can't be empty.\nUsage: /latest <device>")
 
+def changelog(bot, update):
+    cl_text = requests.get("https://raw.githubusercontent.com/PotatoProject/vendor_potato/baked-release/CHANGELOG.md").text.replace("# Changelog\n", "")
+    cl_array = cl_text.split("\n\n### ")
+
+    bot.send_message(chat_id=update.message.chat_id, text="Latest release changelog:\n" + cl_array[1])
+
 def checkUpdates(update_string, device):
     payload = {'device': device, 'type': 'weekly'}
     r = requests.get("https://api.potatoproject.co/checkUpdate", params=payload)
@@ -47,5 +53,8 @@ def checkUpdates(update_string, device):
 
 latest_handler = CommandHandler('latest', latest)
 dispatcher.add_handler(latest_handler)
+
+changelog_handler = CommandHandler('changelog', changelog)
+dispatcher.add_handler(changelog_handler)
 
 updater.start_polling()
